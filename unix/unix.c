924ca271daab7d752d05ee6adf039bab45132fdb
pmqs <pmqs@cpan.org>
2023-10-18 11:02:53 +0100

    patch-unix_unix.c
    Origin:  https://github.com/freebsd/freebsd-ports/tree/main/archivers/unzip

--- unix/unix.c
+++ unix/unix.c
@@ -613,7 +613,7 @@ int mapname(__G__ renamed)
                  * else allow 8-bit characters (e.g. UTF-8) in filenames:
                  */
                 if (uO.cflxflag ||
-                    (isprint(workch) || (128 <= workch && workch <= 254)))
+		    (isprint(workch) || (128 <= workch && workch <= 255)))
                     *pp++ = (char)workch;
         } /* end switch */
 
