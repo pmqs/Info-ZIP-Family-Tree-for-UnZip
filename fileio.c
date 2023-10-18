ffcb780f632b76d1d193cd18c7ae98ec8c6d1f8f
pmqs <pmqs@cpan.org>
2023-10-18 11:10:00 +0100

    extra-ko-patch-fileio.c
    Origin:  https://github.com/freebsd/freebsd-ports/tree/main/archivers/unzip

--- fileio.c
+++ fileio.c
@@ -2240,8 +2240,9 @@ int do_string(__G__ length, option)   /* return PK-type error code */
 
         /* translate the Zip entry filename coded in host-dependent "extended
            ASCII" into the compiler's (system's) internal text code page */
-        Ext_ASCII_TO_Native(G.filename, G.pInfo->hostnum, G.pInfo->hostver,
-                            G.pInfo->HasUxAtt, (option == DS_FN_L));
+	if (!uO.dotflag)
+	    Ext_ASCII_TO_Native(G.filename, G.pInfo->hostnum, G.pInfo->hostver,
+				G.pInfo->HasUxAtt, (option == DS_FN_L));
 
         if (G.pInfo->lcflag)      /* replace with lowercase filename */
             STRLOWER(G.filename, G.filename);
