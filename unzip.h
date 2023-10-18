233bf4e092b7a2d0379172c6766e39adf373d313
pmqs <pmqs@cpan.org>
2023-10-18 11:10:01 +0100

    extra-ko-patch-unzip.h
    Origin:  https://github.com/freebsd/freebsd-ports/tree/main/archivers/unzip

--- unzip.h
+++ unzip.h
@@ -559,6 +559,7 @@ typedef struct _UzpOpts {
 #ifdef UNIX
     int cflxflag;       /* -^: allow control chars in extracted filenames */
 #endif
+    int dotflag;	/* -.: don't translate filenames to local charset */
 #endif /* !FUNZIP */
 } UzpOpts;
 
